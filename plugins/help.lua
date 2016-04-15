cfa8b337a6c13dac763ba97f3516537dbadcf454
parsa13881 <mohammadpa87@gmail.com>
2016-04-14 22:01:05 -0700

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -4,7 +4,6 @@ function run(msg, matches)
 
 local mods = [[ 
 راهنما فارسی مدیران :
-@telemanager_ch
 
 !kick [Username | ID | Reply]
 
@@ -65,7 +64,7 @@ member
  local admin = [[ 
  
  راهنمای فارسی ادمین ها :
- @telemanager_ch
+
  
  !banall [Reply | ID | Username]
  
@@ -108,7 +107,6 @@ member
  local owner = [[
  
  راهنمای فارسی صاحبان گروه :
- @telemanager_ch
  
  !promote [Reply | ID | Username]
  
@@ -141,7 +139,6 @@ member
  local member = [[
  
  راهنمای فارسی عضو ها :
- @telemanager_ch
  
  !calc [formula]
  
@@ -187,7 +184,6 @@ member
  local sudo = [[
  
  راهنما فارسی سودو ها :
- @telemanager_ch
  
  !addadmin [ID | Username]
  
