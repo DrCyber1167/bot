9d9fc0a999d9b8a780a5bfc8bf855232e8edf79b
parsa13881 <mohammadpa87@gmail.com>
2016-04-14 21:59:33 -0700

    Update copyright.lua

--- plugins/copyright.lua
+++ plugins/copyright.lua
@@ -1,26 +1,16 @@
 do
 
 local function run(msg, matches)
-  if matches[1]:lower() == 'telemanager' or 'telemanagerplus' then --change this with anything you want
-    send_document(get_receiver(msg), "./data/me/telemanager.webp", ok_cb, false) --put here sticker location(if you want)
+  if matches[1]:lower() == 'firebot or fire' then --change this with anything you want
+    send_document(get_receiver(msg), "./data/me/fire.webp", ok_cb, false) --put here sticker location(if you want)
       return [[ 
+  Fire Bot
   
-TeleManagerPlus (Open Source) V 3.0.0
-    Advanced ManagerBot Based On TeleSeed
-    
-    Edited By : @ali_ghoghnoos
-    
-    Our Telegram Channel ID :
-    @Telemanager_ch
   
-    Speacial Thanks To My friends :
-    @ThisIsArman
-    @minaco
-    @dragon_born
-    and other that help me for this Bot
-     GitHub :
-     Https://github.com/telemanagerplus/TeleManagerPlus
+  by parsa alemi 
+  
 
+advanced telegram bot based on cli
 
  ]]
   end
@@ -28,11 +18,11 @@ end
 
 return {
   patterns = {
-    "^[!/]([Tt]elemanager)$",
-    "^([Tt]elemanager)$",
-    "^[!/]([Tt]elemanagerplus)$",
-    "^([Tt]elemanagerplus)$",
-    "^(@telemanagerplus)$",
+    "^[!/]([Ff]ire)$",
+    "^([Ff]ire)$",
+    "^[!/]([Ff]irebot)$",
+    "^([Ff]irebot)$",
+    "^(@firebot)$",
     
     },
   run = run
